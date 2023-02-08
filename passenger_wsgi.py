cdec643443f1fb0781c196005279cfe161f5b0ed
joseph <kigozijosefed1993@gmail.com>
2023-02-08 11:47:08 +0300

    nothing much

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -4,4 +4,11 @@ from testproject.wsgi import application
 
 sys.path.insert(0, os.path.dirname(__file__))
 
-environ = os.environ.setdefault("DJANGO_SETTINGS_MODULE", "testproject.settings")
+
+# environ = os.environ.setdefault("DJANGO_SETTINGS_MODULE", "testproject.settings")
+def applications(environ, start_response):
+    start_response('200 OK', [('Content-Type', 'text/plain')])
+    message = 'It Works!\n'
+    version = 'Python %s\n' % sys.version.split()[0]
+    response = '\n'.join([message, version])
+    return [response.encode()]
